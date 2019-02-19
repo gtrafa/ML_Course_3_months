f4475082dfe51dcee1a47892fc3d3b63f51b5d62
gtrafa <gustavo.viagem@gmail.com>
2019-02-19 11:50:14 -0300

    fix spelling mistakes and add another element for testing

--- Python4DS/classOne/IntroductionPyDS.py
+++ Python4DS/classOne/IntroductionPyDS.py
@@ -1,16 +1,18 @@
+# simple classifier to predict the gender based on the three body measures
+# class one  ML to DL
 from sklearn import tree
 
 #[height, weight, shoe size]
 X =[[181,80,44],[177,70,43], [160,60,38],[154,54,37],[166,65,40], [190,90,47],
-[175,64,39],[1777,70,40],[159,70,39], [171,75,42],[181,85,43]]
+[175,64,39],[177,70,40],[159,55,37], [171,75,42],[181,85,43]]
 
 Y = ['male','female','female','female','male','male',
 'male','female','male','female','male']
 
 
-clf = tree.DecisionTreeCllassifier()
+clf = tree.DecisionTreeClassifier()
 clf = clf.fit(X,Y)
 
-prediction = clf.predict([190,70,43])
+prediction = clf.predict([[190,70,43], [177,69,39]])
 
-print (prediction)
+print(prediction)
